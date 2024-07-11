4ab50a4c42cbb2aa72cb981069b613ef547fb79e
Azam0786 <azamsiddique0786@gmail.com>
2024-07-11 18:37:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Mohd Azam Siddique",
+  title: "Web Developer & Content Creator",
+  email: "azamsiddique0786@gmail.com",
+  gitHub: "Azam0786",
+  instagram: "azam___siddique",
+  linkedIn: "mohd-azam-siddique-953712222",
+  medium: "",
+  twitter: "azam___siddique",
+  youTube: "",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
